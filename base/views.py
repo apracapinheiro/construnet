592f2aa4474926c168c00dfefbc23a2389ddc20e
Andre Praca <apracapinheiro@gmail.com>
2018-05-17 17:37:30 -0300

    criando o index do sistema

--- base/views.py
+++ base/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+
+class IndexView(View):
+    """Template view index"""
+    template_name = "index.html"
+    context_object_name = "pagina_inicial"
+
+    def get(self, request):
+        return render(request, 'index.html')
