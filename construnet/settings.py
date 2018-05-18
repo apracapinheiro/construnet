763228e66172f0a93dcb48236ddd9f26993540c4
Andre Praca <apracapinheiro@gmail.com>
2018-05-18 11:41:08 -0300

    inserindo os arquivos static, bootstrap4

--- construnet/settings.py
+++ construnet/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+LOCAL = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(LOCAL, 'static')
+
+MEDIA_ROOT = os.path.join(LOCAL, 'media')
+
+LOGIN_URL = '/login/'
